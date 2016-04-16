f90d51f61deaad60a28f108975439c18887f76b5
Luke Parker <moongateuk@gmail.com>
2016-04-16 15:47:52 +0100

    Added TitleScene

---
+++ LD35/shapeshift/Assets/Scripts/Interactions/SpaceChange.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceChange : MonoBehaviour {
+
+	public int ToScene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.Space)){
+			GameManager.Instance.LoadLevel(ToScene);
+		}
+	}
+}
---
+++ LD35/shapeshift/Assets/Scripts/Interactions/TimedChange.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedChange : MonoBehaviour {
+
+	public float MaxWaitTime = 2.0f;
+	public int SceneNumber = 1;
+	private float _currentTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_currentTime >= MaxWaitTime){
+			GameManager.Instance.LoadLevel(SceneNumber);
+		}
+		else{
+			_currentTime += Time.deltaTime;
+		}
+	}
+}
