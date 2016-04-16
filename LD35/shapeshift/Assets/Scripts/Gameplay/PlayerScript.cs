2d19b4321a183cf1240ba2794f841176b75c415a
Luke Parker <moongateuk@gmail.com>
2016-04-16 16:52:42 +0100

    Added PlayerScript.

---
+++ LD35/shapeshift/Assets/Scripts/Gameplay/PlayerScript.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerScript : MonoBehaviour {
+
+	private Rigidbody2D _rigidBody;
+	//Speed Stuff
+	private float _speedMultiplier = 100.0f;
+	public float Speed = 1.0f;
+
+	//Jump Stuff
+	private float _jumpMultiplier = 500.0f;
+	public float JumpAmount = 1.0f;
+
+	private bool _canDoubleJump = false;
+	private int _jumpCount = 0;
+	const int JUMPS_MAX = 2;
+
+	//Shape Shifting Stuff
+	public string CurrentShape;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InputCheck();
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if(collision.transform.position.y < transform.position.y){
+			_jumpCount = 0;
+		}
+	}
+
+	void InputCheck(){
+		Move(Input.GetAxis("Horizontal"));
+		if(Input.GetAxis("Vertical") > 0.0f){
+			Jump();
+		}
+
+		//TODO: Consider changing to mappable thing...
+		if(Input.GetKeyUp(KeyCode.Alpha1)){
+			ShapeShift("Square");
+		}
+		else if(Input.GetKeyUp(KeyCode.Alpha2)){
+			ShapeShift("Circle");
+		}
+		else if(Input.GetKeyUp(KeyCode.Alpha3)){
+			ShapeShift("Triangle");
+		}
+
+	}
+
+	void Move(float axisAmount){
+		_rigidBody.AddForce(transform.right * (_speedMultiplier * Speed) * axisAmount,ForceMode2D.Force);
+
+		//TODO: Add Animation part to this.
+	}
+
+	void Jump(){
+		if(_jumpCount == 0){
+			_rigidBody.AddForce(transform.up * (_jumpMultiplier * JumpAmount ),ForceMode2D.Impulse);
+			_jumpCount++;
+		}
+		else if(_canDoubleJump){
+			if(JUMPS_MAX < _jumpCount){
+				_rigidBody.AddForce(transform.up * (_jumpMultiplier * JumpAmount ),ForceMode2D.Impulse);
+				_jumpCount++;
+			}
+		}
+	}
+
+	public void ShapeShift(string toWhat){
+		CurrentShape = toWhat;
+		switch(toWhat){
+		case("Square"):
+			Speed = 1.0f;
+			Jump = 1.0f;
+			_canDoubleJump = false;
+			break;
+		case("Circle"):
+			Speed = 2.0f;
+			Jump = 0.2f;
+			_canDoubleJump = false;
+			break;
+		case("Trianlgle"):
+			Speed = 0.75f;
+			Jump = 1.2f;
+			_canDoubleJump = true;
+			break;
+		default:
+			CurrentShape = "Square";
+			Speed = 1.0f;
+			Jump = 1.0f;
+			_canDoubleJump = false;
+			Debug.Log("Defaulted to Square.");
+			break;
+		}
+	}
+}
