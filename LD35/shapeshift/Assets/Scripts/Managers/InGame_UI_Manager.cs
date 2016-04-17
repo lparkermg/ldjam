2222ecd5023391a1afc2d09962c88eb851a74d8d
Luke Parker <moongateuk@gmail.com>
2016-04-17 10:00:55 +0100

    Added InGame UI Manager.

---
+++ LD35/shapeshift/Assets/Scripts/Managers/InGame_UI_Manager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InGame_UI_Manager : MonoBehaviour {
+
+	public CanvasGroup BustedScreen;
+	public CanvasGroup HUD;
+
+	private bool _bustedShown = false;
+	private float _waitTime =2.5f;
+	private float _currentTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_bustedShown){
+			if(_currentTime >= _waitTime){
+				StartAgain();
+			}
+			else{
+				_currentTime += Time.deltaTime;
+			}
+		}
+	}
+
+	public void ShowBusted(){
+		GameManager.Instance.UpdateIsBusted(true);
+		DisableHUD();
+		LeanTween.alphaCanvas(BustedScreen, 1.0f,1.0f).setOnComplete(() => {
+			BustedScreen.blocksRaycasts = true;
+			BustedScreen.interactable = true;
+			_bustedShown = true;
+		});
+	}
+
+	private void DisableHUD(){
+		HUD.blocksRaycasts = false;
+		HUD.interactable = false;
+	}
+
+	public void EnableHUD(){
+		HUD.blocksRaycasts = true;
+		HUD.interactable = true;
+	}
+
+	private void StartAgain(){
+		GameManager.Instance.LoadLevel(1);
+	}
+}
